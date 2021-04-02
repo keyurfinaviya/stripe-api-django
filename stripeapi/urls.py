5e7f0891cb5ac97b4b6c12838d9e41bcbdfb1c93
keyurfinaviya <finaviyakeyur9@gmail.com>
2021-04-02 14:43:34 +0530

    first commit

---
+++ stripeapi/urls.py
@@ -0,0 +1,29 @@
+"""stripeapi URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from application import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('create-payment-intent/<pk>/', views.StripeIntentView.as_view(), name='create-payment-intent'),
+    path('webhooks/stripe/', views.stripe_webhook, name='stripe-webhook'),
+    path('success/', views.SuccessView.as_view(), name='success'),
+    path('cancel/', views.CancelView.as_view(), name='cancel'),
+    path('', views.ProductLandingPageView.as_view(), name='landing-page'),
+    path('create-checkout-session/<pk>/', views.CreateCheckoutSessionView.as_view(), name='create-checkout-session'),
+
+]
